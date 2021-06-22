a19234b8e5541a98d8a9de139b4bb48bf2e33c16
lapa44 <lapa.apiski@gmail.com>
2021-06-22 15:14:28 +0200

    Added comparing to eps and input result, also cleaned code a little bit.

--- main.cpp
+++ main.cpp
@@ -5,13 +5,8 @@
 
 int main() {
 
-    //std::cout << "Metoda 100064: " << task10_1a(-3, 2, 0.0001, 1) << "\n";
-    //std::cout << "Metoda Newtona: " << task10_1b(2, 0.0001); // zaczynamy od konca przedzialu, wynika to z warunku
-    // ze 2 pochodna musi miec ten sam znak co funkcja.
-
     task10_1(-3, 2, 0.0001, 1);
     task10_2(1, 5, 0.00001);
-    //zamienic jeszcze to tak, zeby n sie zmienialo przy nie osiagnieciu EPS
 
 
     return 0;
--- task10_2.cpp
+++ task10_2.cpp
@@ -7,12 +7,13 @@ double function10_2(double x) {
 
 double prostokaty_10_2(double xp, double xk, int n, double eps, int counter) {
     double dx = (xk-xp) / (double)n;
-
     double rezultat = 0;
-
     for (int i=1; i<=n; i++) {
         counter++;
         rezultat += function10_2(xp + (double)i * dx);
+    }
+    if (rezultat*dx - 124.48 > eps) {
+        return prostokaty_10_2(xp, xk, n*2, eps, counter);
     }
     std::cout << "Liczba operacji w metodzie prostokatow: " << counter << "\n";
     return rezultat * dx;
@@ -25,12 +26,16 @@ double trapezy_10_2(double xp, double xk, int n, double eps, int counter) {
         counter++;
         rezultat += function10_2(xp + (double)i * dx);
     }
+    rezultat = (rezultat + (function10_2(xp) + function10_2(xk)) / 2) * dx;
+    if (rezultat - 124.48 > eps) {
+        return trapezy_10_2(xp, xk, n*2, eps, counter);
+    }
     std::cout << "Liczba operacji w metodzie trapezow: " << counter << "\n";
-    return (rezultat + (function10_2(xp) + function10_2(xk)) / 2) * dx;
+    return rezultat;
 }
 
 void task10_2(double xp, double xk, double eps) {
-    int n = 5; // poczatkowa liczba przedzialow
+    int n = 4; // poczatkowa liczba przedzialow
     std::cout << "Rezultat metoda prostokatow: " << prostokaty_10_2(xp, xk, n, eps, 0) << "\n";
     std::cout << "Rezultat metoda trapezow: "<< trapezy_10_2(xp, xk, n, eps, 0) << "\n"; // 124-125 dobry wynik :D
 }
