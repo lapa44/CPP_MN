1558283e33729fc4c8f38682b29fe641ffd22711
lapa44 <39618410+lapa44@users.noreply.github.com>
2021-06-24 10:17:59 +0200

    cleaned code

--- task10_1.cpp
+++ task10_1.cpp
@@ -17,29 +17,33 @@ double task10_1a(double xp, double xk, double eps, double incrementer, int count
         result = function10_1(i);
         //std::cout << "Przyblizenie nr " << counter << "=\t"  << result << "dla x = " << i << "\n";
         if (result == 0 || std::abs(result) < eps) {
-            std::cout << "Liczba operacji: " << counter << "\n";
+            std::cout << "Liczba operacji metoda 100064: " << counter << "\n";
             return i;
         }
     }
-    return task10_1a(xp, xk, eps, incrementer / 6, counter); // bylo /10, ale liczba operacji była x10
+    //return task10_1a(xp, xk, eps, incrementer / 6, counter); // bylo /10, ale liczba operacji była x10
+    return task10_1a(xp, xk, eps, incrementer/6, counter);
 }
 
 double task10_1b(double x, double eps) {
 
     double f0 = function10_1(x), x0 = x;
+    int counter = 1;
 
     while (fabs(f0) > eps) {
         x = x0;
         x0 = x - f0 / derivative10_1(x);
         f0 = function10_1(x0);
         std::cout << "Przyblizono " << f0 << " dla x = " << x0 << "\n";
+        counter++;
     }
+    std::cout << "Liczba operacji w  metodzie Newtona: " << counter << std::endl;
     return x0;
 }
 
 void task10_1(double xp, double xk, double eps, double incrementer) {
-    std::cout << "Metoda 100064: " << task10_1a(-3, 2, 0.0001, 1) << "\n";
-    std::cout << "Metoda Newtona: " << task10_1b(2, 0.0001);
+    std::cout << "Metoda 100064 wynik: " << task10_1a(xp, xk, eps, incrementer) << "\n";
+    std::cout << "Metoda Newtona wynik: " << task10_1b(2, eps) << std::endl;
     // zaczynamy od konca przedzialu, wynika to z warunku
     // ze 2 pochodna musi miec ten sam znak co funkcja.
 }
